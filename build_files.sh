944eaaf6421c642e1de81951387ee616d237789b
aket0 <moynehubert@gmail.com>
2024-06-12 10:26:56 +0200

    deployment

--- build_files.sh
+++ build_files.sh
@@ -7,12 +7,23 @@ then
     exit 1
 fi
 
+python3 --version
+pip3 --version
+
 # Check if pip3 is installed and install it if necessary
 if ! command -v pip3 &> /dev/null
 then
     echo "pip3 could not be found, installing it now..."
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3 get-pip.py
+fi
+
+# Install PostgreSQL development libraries if using psycopg2 instead of psycopg2-binary
+if ! command -v pg_config &> /dev/null
+then
+    echo "pg_config could not be found, installing PostgreSQL development libraries..."
+    apt-get update
+    apt-get install -y libpq-dev
 fi
 
 # Install virtualenv if not already installed
@@ -24,6 +35,9 @@ fi
 # Create and activate virtual environment
 python3 -m virtualenv venv
 source venv/bin/activate
+
+# Check installed packages
+pip list
 
 # Install dependencies
 pip install -r requirement.txt
