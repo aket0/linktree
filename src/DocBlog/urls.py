a9a6b7cf652102df12cafcbc21529fec4fd93111
aket0 <moynehubert@gmail.com>
2024-06-12 09:42:46 +0200

    deployment

--- src/DocBlog/urls.py
+++ src/DocBlog/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
 ]
 
 handler404 = views.handler404
-
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
