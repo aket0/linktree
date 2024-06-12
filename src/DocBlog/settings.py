22a374b92a92c587473cdcb0a1c918112314a98c
aket0 <moynehubert@gmail.com>
2024-06-12 09:10:15 +0200

    deployment

--- src/DocBlog/settings.py
+++ src/DocBlog/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'PASSWORD': 'FAYfgMfdwOxmfmUTHoHQDDQZgkcwbuhq',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '17719',
     }
 }
 
